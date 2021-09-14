d0214edc5ca43e7885e7c7aa13dc66c843ae3022
VivianRelvas <vivianrelvas@hotmail.com>
2021-09-14 19:51:34 +0000

    Incluindo pasta e arquivo

---
+++ src/main/java/application/controllers/LivroController.java
@@ -0,0 +1,19 @@
+package application.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/livro")
+
+public class LivroController {
+@Autowired
+@RequestMapping("/list")
+
+public String list(Model model) {
+    model.addAttribute("livros");
+    return "list.jsp";       //vamos criar esse arquivo dentro da pasta webapp --> dentro da pasta livro
+}
+}
